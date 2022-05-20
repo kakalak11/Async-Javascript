5dd1a8266478a5a506502bd0734941de89585dea
kakalak11 <92971472+kakalak11@users.noreply.github.com>
2022-05-20 11:13:19 +0700

    - initial commit

---
+++ temp/declarations/cc.d.ts
@@ -0,0 +1,8 @@
+
+    /// <reference path="/Users/fe-anhtu-fresher/Desktop/Gemi/cc30-cocos-engine-tech/engine/bin/.declarations/cc.d.ts"/>
+    
+    /**
+     * @deprecated Global variable `cc` was dropped since 3.0. Use ES6 module syntax to import Cocos Creator APIs.
+     */
+    declare const cc: never;
+    
