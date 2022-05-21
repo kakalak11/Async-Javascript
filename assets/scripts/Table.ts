ee0fcb2cbdd07b9dd4522c62b79ce6e8b530f4f2
kakalak11 <92971472+kakalak11@users.noreply.github.com>
2022-05-21 22:58:59 +0700

    Revert "- finish quest 1"
    This reverts commit 3e0c2198b138ecbe53183cb8b273c42f7580aedb.

--- assets/scripts/Table.ts
+++ assets/scripts/Table.ts
@@ -8,7 +8,6 @@ const { ccclass, property } = _decorator;
 export class Table extends Component {
 
     _reels = [];
-    _promiseList = [];
 
     @property({type: Label})
     status = null;
@@ -19,20 +18,7 @@ export class Table extends Component {
 
     spin() {
         this.status.string = 'Table Spinning';
-        this._reels.forEach(reel => {
-            const resolve = () => true;
-            const promise = new Promise(resolve => {
-                reel.startSpin(resolve);
-            })
-            this._promiseList.push(promise);
-        });
-        const callbackComplete = this.onTableStop.bind(this);
-        Promise.all(this._promiseList).then(callbackComplete);
-    }
-
-    onTableStop() {
-        this.status.string = 'Table Stopped';
-        console.log('Table Stop');
+        this._reels.forEach(it => it.startSpin());
     }
 
     /*
